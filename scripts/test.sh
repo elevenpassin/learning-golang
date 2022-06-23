a90c9b57578c67d73ea0cf5e0bbff0ae09362929
Alice Kile <buoyantair@protonmail.com>
2022-06-23 23:06:56 +0200

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,11 @@
+cd ./packages/
 for dir in ./*/     # list directories in the form "/tmp/dirname/"
 do
     dir=${dir%*/}      # remove the trailing "/"
     dirname=${dir##*/} # get everything after the final "/"
     echo "building ${dirname}..."
     cd ${dirname}
+    pwd
     go test -v .
     cd ..
 done
