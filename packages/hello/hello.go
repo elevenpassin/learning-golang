7b012ed27d6b5bff26d3a286acc293fa7e36dd6e
Alice Kile <buoyantair@protonmail.com>
2022-06-23 22:43:56 +0200

    update

---
+++ packages/hello/hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/buoyantair/learning-golang/packages/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, message := range messages {
+		fmt.Println(message)
+	}
+}
